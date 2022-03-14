d431b3822ae5f935ae6edaf982e908db15b73402
Vlad Lialin <Guitaricet@gmail.com>
2022-03-14 17:58:12 -0400

    Update main.py

--- main.py
+++ main.py
@@ -11,10 +11,10 @@ from life_after_bert.data import load_olmpics_data, MCDataset
 def get_args():
     """ Set hyperparameters """
     parser = argparse.ArgumentParser()
-    parser.add_argument("model_name", help="Identifier of any pretrained transformers.AutoModelForMaskedLM")
-    parser.add_argument("task", help="Type of task, among `oLMpics MLM`")  # TODO: add more tasks
-    parser.add_argument("data_path", help="Path to jsonl file containing dataset questions")  # TODO: Hug Hub dataset
-    parser.add_argument("num_choices", help="Number of answer choices for each question", type=int)
+    parser.add_argument("--model_name", help="Identifier of any pretrained transformers.AutoModelForMaskedLM")
+    parser.add_argument("--task", help="Type of task, among `oLMpics MLM`")  # TODO: add more tasks
+    parser.add_argument("--data_path", help="Path to jsonl file containing dataset questions")  # TODO: Hug Hub dataset
+    parser.add_argument("--num_choices", help="Number of answer choices for each question", type=int)
     return parser.parse_args()
 
 

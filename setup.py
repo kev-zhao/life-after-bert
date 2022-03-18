dd380b74dc0ee40b38ec5f4b31aa7e9974443361
Vlad Lialin <Guitaricet@gmail.com>
2022-03-18 17:21:38 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,11 @@
 from setuptools import setup
 
+with open("requirements.txt") as f:
+    requirements = f.read().strip().splitlines()
+
 setup(
     name="life_after_bert",
     version="0.1",
     url="https://github.com/kev-zhao/life-after-bert",
+    setup_requires=requirements,
 )
